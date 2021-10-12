33c909f1325cbc619556b05d5c32ec8939fe7c4e
enoch wu <enoch.wu@kingsway.school.nz>
2021-10-12 14:06:20 +1300

    Added a fake sales tag

--- script.js
+++ script.js
@@ -194,7 +194,7 @@ function sendOrder() {
 }
 //thanks to w3schools.com for the countdown code
 // Set the date we're counting down to
-var countDownDate = new Date("October 12, 2021 13:37:25").getTime();
+var countDownDate = new Date("October 20, 2021 13:37:25").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function () {
