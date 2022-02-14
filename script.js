8abe0581aadc4217d0e2a798e4cafcad530bd3e1
MM4096 <85976396+MM4096@users.noreply.github.com>
2022-02-15 10:49:26 +1300

    q

--- script.js
+++ script.js
@@ -144,11 +144,10 @@ function fillIn() {
 			let str = $("#cItemCost" + k).text();
 			str = str.substring(1);
 			console.log(str);
-			iCost = parseInt(toString(str));
+			let iCost = parseInt(str);
 			console.log(iCost);
 			cCost += iCost;
 			console.log("cCost = " + cCost);
-
 	}
 	$("#cTotal").text("$" + cCost);
 }
