b6264eda3d1745f6afc991b6a5c1a80f9e855141
MM4096 <85976396+MM4096@users.noreply.github.com>
2022-02-16 20:01:25 +1300

    nV0.7 - Added tags into the search function

--- jquery.js
+++ jquery.js
@@ -5,7 +5,7 @@ $(document).ready(function () {
             $("#qty").val(0);
         }
     })
-    
+
     $("#cost").click(function () {
         var txt = $("#cost").text();
         if (added == 1) {
@@ -14,7 +14,7 @@ $(document).ready(function () {
             $("#cost").text("Failed");
         }
         setTimeout(function () { $("#cost").text(txt);}, 1000);
-        
+
     })
     //search function
     //Made by me!
@@ -23,17 +23,33 @@ $(document).ready(function () {
 
 })
 function search() {
-    const items = ["", "chocolate favourites", "chupa chups", "licorice allsorts", "lolly scramble", "mystery box", "sour candy"];
+  $("#iViewItems").children().hide();
+    //const items = ["", "chocolate favourites", "chupa chups", "licorice allsorts", "lolly scramble", "mystery box", "sour candy"];
+    var items = ["", ["chocolate favourites", "favorite", "favourite", "50", "chocolate"],
+      ["chupa chups", "lollipop", "50", "lolly", "lollies"],
+      ["licorice allsorts", "licorice", "1", "lolly", "lollies"],
+      ["lolly scramble", "lolly", "lollies", "1"],
+      ["mystery box", "surprise", "box", "pack", "5", "random"],
+      ["sour candy", "sour", "candy", "lollies", "lolly", "50"]
+
+  ];
     var searchVal = $("#search").val();
-    var searchValue = searchVal.toLowerCase();
-    $("#result").show();
-    $("#result").text("Search results for " + searchVal + ":");
-    for (let i = 1; i < 7; i++) {
-        var id = "o" + i;
-        if (items[i].includes(searchValue)) {
-            $("#" + id).show();
-        } else {
-            $("#" + id).hide();
+    console.log(searchVal);
+    searchVal = searchVal.toLowerCase();
+    items.forEach(function(element, index, array) {
+      console.log(element + ", " + index);
+      let a = items[index].length;
+      for (let i = 0; i <= a; i++) {
+      let currentEvaluation = items[index][i];
+      console.log(currentEvaluation);
+      if (currentEvaluation !== undefined) {
+        if (currentEvaluation.includes(searchVal, 0)) {
+          $(".itemView").eq(index - 1).show();
+          console.log("contains!");
         }
+      }
+
     }
+  });
+
 }
